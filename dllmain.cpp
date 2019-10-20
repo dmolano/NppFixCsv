7a69d0300302e3f95128c014f1820c338dcf3dde
Dionisio Molano Robledo <dmolanor@gmail.com>
2019-10-20 14:06:34 +0200

    Cabeceras de licencia correctas.

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,19 +1,28 @@
-/* This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>. */
-
-// dllmain.cpp : Define el punto de entrada de la aplicación DLL.
+/***************************************************************************/
+/*  This program is free software: you can redistribute it and/or modify   */
+/*	it under the terms of the GNU General Public License as published by   */
+/*	the Free Software Foundation, either version 3 of the License, or      */
+/*	(at your option) any later version.                                    */
+/*                                                                         */
+/*	This program is distributed in the hope that it will be useful,        */
+/*	but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/*	GNU General Public License for more details.                           */
+/*                                                                         */
+/*	You should have received a copy of the GNU General Public License      */
+/*	along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+/*                                                                         */
+/*                                                                         */
+/*  Author: Dionisio Molano Robledo                                        */
+/*  Date: 2019/10/01                                                       */
+/*  Email: dmolano.smriti@gmail.com                                        */
+/*                                                                         */
+/***************************************************************************/
 #include "pch.h"
 
+/*
+ * DllMain
+ */
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
@@ -22,9 +31,13 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		nppFixCsv_PluginDllProcessAttach(hModule);
+		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
+		break;
 	case DLL_PROCESS_DETACH:
+		nppFixCsv_PluginDllProcessDetach();
 		break;
 	}
 	return TRUE;
--- pch.cpp
+++ pch.cpp
@@ -1,17 +1,23 @@
-/* This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>. */
-
-// pch.cpp: el archivo de código fuente correspondiente al encabezado precompilado
+/***************************************************************************/
+/*  This program is free software: you can redistribute it and/or modify   */
+/*	it under the terms of the GNU General Public License as published by   */
+/*	the Free Software Foundation, either version 3 of the License, or      */
+/*	(at your option) any later version.                                    */
+/*                                                                         */
+/*	This program is distributed in the hope that it will be useful,        */
+/*	but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/*	GNU General Public License for more details.                           */
+/*                                                                         */
+/*	You should have received a copy of the GNU General Public License      */
+/*	along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+/*                                                                         */
+/*                                                                         */
+/*  Author: Dionisio Molano Robledo                                        */
+/*  Date: 2019/10/01                                                       */
+/*  Email: dmolano.smriti@gmail.com                                        */
+/*                                                                         */
+/***************************************************************************/
 
 #include "pch.h"
 
