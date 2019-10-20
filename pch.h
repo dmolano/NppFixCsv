34a41e9441a127fdd39a002479fc55c8122d6785
dmolanor <dmolanor@gmail.com>
2019-10-20 09:42:48 +0200

    Cabeceras de documentación.

---
+++ pch.h
@@ -0,0 +1,26 @@
+/* This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+// pch.h: este es un archivo de encabezado precompilado.
+// Los archivos que se muestran a continuación se compilan solo una vez, lo que mejora el rendimiento de la compilación en futuras compilaciones.
+// Esto también afecta al rendimiento de IntelliSense, incluida la integridad del código y muchas funciones de exploración del código.
+// Sin embargo, los archivos que se muestran aquí se vuelven TODOS a compilar si alguno de ellos se actualiza entre compilaciones.
+// No agregue aquí los archivos que se vayan a actualizar con frecuencia, ya que esto invalida la ventaja de rendimiento.
+
+#ifndef PCH_H
+#define PCH_H
+
+// agregue aquí los encabezados que desea precompilar
+#include "framework.h"
+
+#endif //PCH_H
